58fbf7311fe2b7ea2018f2f4b3ea27a131e6247a
Mark Morley <mmorley0395@gmail.com>
2023-10-12 09:31:03 -0400

    added regional index to speed up dvrpc_reg queries

--- loder_components/db_update.py
+++ loder_components/db_update.py
@@ -12,8 +12,7 @@ PW = os.getenv("PW")
 
 def db_connect(db: str = "postgres"):
     """Boilerplate for connection params"""
-    conn = psycopg2.connect(
-        dbname=db, user=UN, password=PW, host=HOST, port=PORT)
+    conn = psycopg2.connect(dbname=db, user=UN, password=PW, host=HOST, port=PORT)
     conn.autocommit = True
     cursor = conn.cursor()
     return cursor, conn
@@ -22,35 +21,38 @@ def db_connect(db: str = "postgres"):
 def build_index(lode_no: str, counties: list, year: int):
     """Build an index to speed up later queries"""
     cursor, conn = db_connect(lode_no)
-    d = {'rac': 'h_geocode', 'wac': 'w_geocode'}
+    d = {"rac": "h_geocode", "wac": "w_geocode"}
     for key, value in d.items():
-        print(f'building index for {key} table...')
+        print(f"building index for {key} table...")
         q = f"""create index if not exists {key}_index on {key}.combined_{key}_table({value});"""
-        cursor.execute(q, {'counties': counties})
-    print('building index for geography crosswalk...')
+        cursor.execute(q, {"counties": counties})
+    print("building index for geography crosswalk...")
     cursor.execute(
-        f"""create index if not exists xwalk_index on geo_xwalk.xwalk(tabblk{year})""")
-    print('building index for od table...')
+        f"""create index if not exists xwalk_index on geo_xwalk.xwalk(tabblk{year})"""
+    )
+    print("building index for od table...")
     cursor.execute(
-        'create index if not exists idx_home on od.combined_od_table(h_geocode);')
+        "create index if not exists idx_home on od.combined_od_table(h_geocode);"
+    )
     cursor.execute(
-        'create index if not exists idx_work on od.combined_od_table(w_geocode);')
+        "create index if not exists idx_work on od.combined_od_table(w_geocode);"
+    )
     cursor.close()
     conn.close()
 
 
 def local_flag(lode_no: str, year: int, counties: list):
     """Sets the regional identifies column (dvrpc_reg) to true for the counties
-       list passed into the class (default is dvrpc counties). For OD, the the flag
-       is set for either/both home/work blocks where the block is in self.counties."""
+    list passed into the class (default is dvrpc counties). For OD, the the flag
+    is set for either/both home/work blocks where the block is in self.counties."""
     cursor, conn = db_connect(lode_no)
-    tables = ['rac', 'wac', 'od']
-    cols = ['w_geocode', 'h_geocode']
+    tables = ["rac", "wac", "od"]
+    cols = ["w_geocode", "h_geocode"]
     for table in tables:
         init_q = f"update {table}.combined_{table}_table SET dvrpc_reg = false"
         cursor.execute(init_q)
-        if table == 'rac' or table == 'wac':
-            census_block_col = cols[1] if table == 'rac' else cols[0]
+        if table == "rac" or table == "wac":
+            census_block_col = cols[1] if table == "rac" else cols[0]
             q = f"""update {table}.combined_{table}_table
                     set dvrpc_reg = case
                       when xwalk.ctyname = ANY(%(counties)s) then true
@@ -60,13 +62,12 @@ def local_flag(lode_no: str, year: int, counties: list):
                     where {table}.combined_{table}_table.{census_block_col} = geo_xwalk.xwalk.tabblk{year}
                     and xwalk.ctyname = ANY(%(counties)s)
                 """
-            print(f'updating dvrpc_reg column in {table}...')
-            cursor.execute(q, {'counties': counties})
+            print(f"updating dvrpc_reg column in {table}...")
+            cursor.execute(q, {"counties": counties})
 
-        elif table == 'od':
+        elif table == "od":
             for col in cols:
-                print(
-                    f'updating dvrpc_reg column in {table} for column {col}...')
+                print(f"updating dvrpc_reg column in {table} for column {col}...")
                 q = f"""update {table}.combined_{table}_table
                         set dvrpc_reg = case
                           when xwalk.ctyname = ANY(%(counties)s) then true
@@ -76,7 +77,19 @@ def local_flag(lode_no: str, year: int, counties: list):
                         where {table}.combined_{table}_table.{col} = geo_xwalk.xwalk.tabblk{year}
                         and xwalk.ctyname = ANY(%(counties)s)
                     """
-                cursor.execute(q, {'counties': counties})
+                cursor.execute(q, {"counties": counties})
 
+    cursor.close()
+    conn.close()
+
+
+def build_regional_index(lode_no: str):
+    """Build an index to speed up later queries"""
+    cursor, conn = db_connect(lode_no)
+    tables = ["rac", "wac", "od"]
+    for table in tables:
+        print(f"building regional index for {table} table...")
+        q = f"""create index if not exists regional_{table}_index on {table}.combined_{table}_table(dvrpc_reg);"""
+        cursor.execute(q)
     cursor.close()
     conn.close()
